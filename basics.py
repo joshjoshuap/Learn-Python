12e963f597d13b39817a688bca708f8403e69184
joshjoshuap <joshjoshuap1@gmail.com>
2024-01-07 15:57:07 +0800

    basic strings methods & slicing

--- basics.py
+++ basics.py
@@ -18,22 +18,59 @@ subtraction = number2 - number3
 multiplication = number1 * number2 * number3
 division = number3 / number1
 modulo = number2 % number3
-remainder = 23 % 2 # 1
-noRemainder = 22 % 2 # 0
-power = 2 ** 3 # 2 * 2 * 2 = 8
-multiples = 2 + 3 * 4 + 5 # 19
-specifics = (2 + 3) * (4 + 5) # 45
+remainder = 23 % 2 
+noRemainder = 22 % 2 
+power = 2 ** 3 
+multiples = 2 + 3 * 4 + 5 
+specifics = (2 + 3) * (4 + 5) 
 
-# STRINGS
-stringLength = len("This will count the length of a string")
+# print(remainder) # 1
+# print(noRemainder) # 0
+# print(power) # 2 * 2 * 2 = 8
+# print(multiples) # 19
+# print(specifics) # 45
+
+# STRING INDEXING / SLICING
 indexingString = "Hello World"
 slicingString = "Josh Joshua"
 stepSlicingString = '123456789'
 
-print(stringLength)
-print(indexingString[0]) # first letter
-print(indexingString[-1]) # last letter
-print(slicingString[5:]) # Joshua
-print(slicingString[0:4]) # Josh
-print(stepSlicingString[::2]) # 13579
-print(stepSlicingString[0:5:2]) # 135
+# print(indexingString[0]) # first letter
+# print(indexingString[-1]) # last letter
+# print(slicingString[5:]) # Joshua
+# print(slicingString[0:4]) # Josh
+# print(stepSlicingString[::2]) # 13579
+# print(stepSlicingString[0:5:2]) # 135
+
+# STRING METHODS
+firstName = 'Joshua'
+lastname = 'Pautanes'
+helloWorld = 'Hello World'
+multipleLetter = 'J' * 10
+
+stringConcat = firstName + ' ' + lastname
+lowerCase = firstName.lower()
+upperCase = lastname.upper()
+stringLength = len(firstName)
+splitString = helloWorld.split();
+
+# print(stringLength) # 6
+# print(stringConcat) # Joshua Pautanes
+# print(multipleLetter) # JJJJJJJJJJ
+# print(lowerCase) # joshua
+# print(upperCase) # PAUTANES
+# print(splitString) # ['Hello', 'World']
+
+# String Formatting
+name = 'Joshua'
+age = 23
+address = 'Quezon City'
+pi = 3.14159265359
+
+introduction = 'My name is {} {} years old from {}'.format(name, age, address)
+introduction2 = 'My name is {name} from {address} and {age} years old'.format(name = 'Jose', age = 99, address = 'Manila')
+shortenedPI = 'The PI is {total:1.3f}'.format(total=pi)
+
+# print(introduction) # My name is Joshua 23 years old from Quezon City
+# print(introduction2) # My name is Jose from Manila and 99 years old
+# print(shortenedPI) # The PI is 3.142
