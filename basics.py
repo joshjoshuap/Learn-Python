cb8e4eeca8d379a4e3ff006535c75792bf2571f0
joshjoshuap <joshjoshuap1@gmail.com>
2024-01-07 14:53:10 +0800

    data types & arithmetic basics

---
+++ basics.py
@@ -0,0 +1,32 @@
+# Data Types
+number = 1
+float = 5.5
+string = 'Joshua'
+StringList = ['Apple', 'Banana', 'Orange']
+NumberList = [1,2,3]
+CombinedList = ['one', 2, 'three']
+Dictionaries = {'firstname' : 'Joshua', 'age': 24}
+isTrue = True
+
+# print(StringList[0])
+# print(Dictionaries['firstname'])
+
+
+# Arithmetic
+number1 = 5
+number2 = 8
+number3 = 10
+
+addition = number1 + number2
+subtraction = number2 - number3
+multiplication = number1 * number2 * number3
+division = number3 / number1
+modulo = number2 % number3
+remainder = 23 % 2 # 1
+noRemainder = 22 % 2 # 0
+power = 2 ** 3 # 2 * 2 * 2 = 8
+multiples = 2 + 3 * 4 + 5 # 19
+specifics = (2 + 3) * (4 + 5) # 45
+
+print(multiples)
+print(specifics)
