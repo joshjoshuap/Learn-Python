1410f532469b9a3ce56af0ce5917dba0a9c40c27
Joshua <joshjoshuap1@gmail.com>
2024-01-08 19:39:13 +0800

    looping

---
+++ loop.py
@@ -0,0 +1,28 @@
+numberLists = [1,2,3,4,5,6,7,8,9]
+
+# WHILE
+x = 0
+while x <= 5:
+    print(x)
+    x += 1
+
+# FOR LOOP
+# for num in numberLists:
+#     print(num)
+
+# FOR IF ELSE 
+# for evenNumber in numberLists:
+#     if evenNumber % 2 == 0:
+#         print(evenNumber) 
+
+# ADDING BY LOOPING
+# addNumbers = 0
+# for number in numberLists:
+#     addNumbers = addNumbers + number
+# print(addNumbers) # 45
+
+# TUPLE LOOPING
+# myListTuple = [(1,2), (3,4), (5,6)]
+# for a,b in myListTuple:
+#     print (a) # only display the first index of tuple
+#     print (a,b)
