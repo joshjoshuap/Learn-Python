67abadcc0519fcbc9b1cb695bc1f04c49e200a2e
Joshua <joshjoshuap1@gmail.com>
2024-03-10 17:34:42 +0800

    learning set

--- basics.py
+++ basics.py
@@ -118,4 +118,4 @@ sampleNumberLists[0] = 'One'
 listOfNumbers = [1,1,1,2,3,3,4,5,6,6,6,7,7,8,9,9,9]
 newSet = set(listOfNumbers) # remove repeatedly value in lists
 
-# print(newSet)
+print(newSet)
