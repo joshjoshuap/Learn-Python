2089ccbe468f24e9b0056f7f4c587aba86b873e0
Joshua <joshjoshuap1@gmail.com>
2024-07-14 18:02:50 +0800

    multi function

--- functions.py
+++ functions.py
@@ -5,3 +5,18 @@ def sample_Function():
     print(f'{firstName} {lastName}')
 
 sample_Function()
+
+# Calling Functions
+def num1():
+    num1 = 5
+    return num1
+
+def num2():
+    num2 = 10
+    return num2
+
+def addNumbers():
+    total = num1() + num2()
+    return total
+
+print(addNumbers())
