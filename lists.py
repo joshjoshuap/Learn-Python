0036dcef7ab25854c6f80dc5ea4e1d3a2677aba7
Josh Joshua <joshjoshuap.com>
2025-01-14 21:46:25 +0800

    learn

--- lists.py
+++ lists.py
@@ -7,15 +7,16 @@ randomNumberLists = [2,1,4,7,6,8]
 firstLists[0] = 'One'
 secondLists.pop() # [5, 6] 
 secondLists.append(8)  # [5, 6, 8] 
+secondLists.insert(0, 4) # [4, 5, 6, 8]
 randomNumberLists.sort() # sorting list
 sortedList = randomNumberLists # calling the sorted list
 
 # print(combinedLists) # [1, 2, 3, 5, 6, 7]
 # print(firstLists) # ['One', 2, 3]
-# print(secondLists)
+print(secondLists)
 # print(sortedList) # [1, 2, 4, 6, 7, 8]
 
 # NESTED LISTS / MULTIPLE LIST
 nestedlist = [firstLists, secondLists]
-print(nestedlist) #[['One', 2, 3], [5, 6, 8]]
-print(nestedlist[0][0]) # One
+# print(nestedlist) #[['One', 2, 3], [5, 6, 8]]
+# print(nestedlist[0][0]) # One
