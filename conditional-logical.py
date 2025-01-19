528f5f58483a301995b0607245ce07f9a5c813b9
Joshua <joshjoshuap1@gmail.com>
2025-01-19 18:13:00 +0800

    learn conditional

--- conditional-logical.py
+++ conditional-logical.py
@@ -32,3 +32,12 @@ elif userAge <= 18 and userLicensed == True:
     print("You're driver license is invalid, cause your a minor")
 else:
     print('You are not allowed to take license and drive')
+    
+# Tenary Operator
+userAge2 = 16 
+userLicensed2 = True
+if userAge2 >= 18 and userLicensed == True: print('You are allowed to driver')
+if userAge2 <= 18 and userLicensed == True: print("You're driver license is invalid, cause your a minor")
+if userAge2 >= 18 and userLicensed == False: print('You need a driver license first')
+if userAge2 <= 18 and userLicensed == False: print('You are not allowed to take license and drive')
+
