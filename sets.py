6cb8a6f0a69b0b091c498f1c8396ea6633a9dc4b
Joshua <joshjoshuap1@gmail.com>
2025-01-19 17:02:22 +0800

    learn sets

--- sets.py
+++ sets.py
@@ -1,5 +1,11 @@
 # SET
-listOfNumbers = [1,1,1,2,3,3,4,5,6,6,6,7,7,8,9,9,9]
+listOfNumbers = [1,1,1,2,3,3,4,5,6,6,6,7,7,8]
 newSet = set(listOfNumbers) # remove repeatedly value in lists
+# {1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(newSet) 
 
-print(newSet)
+# methods
+firstSet = {1,2,3,4,5}
+secondSet = {4,5,6,7,8,9}
+print(firstSet & secondSet) # {4,5}
+print(firstSet | secondSet) # {1, 2, 3, 4, 5, 6, 7, 8, 9}
