1912b274d3f0383361a2d5e02a6f887060792f6e
Joshua <joshjoshuap1@gmail.com>
2025-01-26 16:22:32 +0800

    learn functions

--- functions.py
+++ functions.py
@@ -21,9 +21,36 @@ def addNumbers():
 
 print(addNumbers())
 
-# Passing Values
+# Nested Function
+def inputNumber(num1, num2):
+    def sum(num1, num2):
+        return num1 + num2
+    return sum(num1, num2)
+
+total = inputNumber(10, 100)
+print(total)
+
+# Paramaters Values
 def name(inputName):
     print(f"Hello {inputName}")
     
-userInput = input("What is your name? ")
-name(userInput)
+# userInput = input("What is your name? ")
+# name(userInput)
+
+# Default Value
+def userProfile(name,age = 0):
+    print(f'Name: {name} Age: {age}')
+    
+name = 'Joshua'    
+# age = 24
+userProfile(name,)
+
+# Docstrings
+def unknownFunction(test):
+    '''
+    >>> This is the function that print
+    '''
+    print(test)
+    
+unknownFunction('Hello')
+print(unknownFunction.__doc__)
